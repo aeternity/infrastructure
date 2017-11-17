fdf878987ea34d51756c1c3a72135c5059b1ed5e
Dincho Todorov <dincho@users.noreply.github.com>
2017-11-17 10:37:30 +0200

    Add ansible setup playbook on openstack infra (#1)
    * Add ansible setup playbook on openstack infra
    * Update README with usage exmaple
    * Limit setup playbook to epoch group only
    * Add velzevur to developers
    * Install dev tools to all epoch nodes
    * Add automatic apt-get update
    * Add more info about dynamic inventory
    * Make it clear that automatic cleanup of authorized_keys is not supported

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ev
+
+# Free Travis version does not support SSH key pairs, thus using encrypted files for SSH deploy key
+# https://docs.travis-ci.com/user/encrypting-files/
+openssl aes-256-cbc -K $encrypted_989f4ea822a6_key -iv $encrypted_989f4ea822a6_iv -in ansible/files/travis/master_key.enc -out master_rsa -d
+chmod 600 /tmp/master_rsa
+eval "$(ssh-agent -s)"
+ssh-add /tmp/master_rsa
+
+# Setup all epoch nodes with Ansible
+cd ansible && ansible-playbook --limit=epoch setup.yml
