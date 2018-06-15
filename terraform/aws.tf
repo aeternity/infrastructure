3d15637630446f6d89e784831098bd0f1ef930db
acid <acid@sennui.com>
2018-06-15 16:59:06 +0200

    disable profile

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,18 +1,18 @@
 terraform {
-  backend "s3" {
-    bucket = "aeternity-terraform-states"
-      key    = "aeternity-test-state.tfstate"
-#    shared_credentials_file = "/aws/credentials"
-      region = "us-east-1"
-#  profile                 = "aeternity"
-  }
+    backend "s3" {
+        bucket = "aeternity-terraform-states"
+        key    = "aeternity-test-state.tfstate"
+        region = "us-east-1"
+#        profile                 = "aeternity"
+#        shared_credentials_file = "/aws/credentials"
+    }
 }
 
 provider "aws" {
   version                 = "1.22"
   region                  = "eu-west-1"
-  shared_credentials_file = "/aws/credentials"
-  profile                 = "aeternity"
+#  shared_credentials_file = "/aws/credentials"
+#  profile                 = "aeternity"
 }
 
 module "fleet" {
