a5806aac97f46ec60746c7e21a707d7a706d049a
acid <acid@sennui.com>
2018-06-18 14:07:06 +0200

    change name

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -20,6 +20,6 @@ module "fleet" {
     static = 1
     spot = 1
     ami_id = "${var.ami_id}"
-    env = "spot"
+    env = "dev3"
     subnets = ["subnet-aa989ef1","subnet-25d11543","subnet-0c7bb044"]
 }
