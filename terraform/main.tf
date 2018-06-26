50001466d72b59228e8c3e320f235cb0e22841b4
acid <acid@sennui.com>
2018-06-26 15:39:29 +0200

    remove profile

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,8 +3,6 @@ terraform {
     bucket                  = "aeternity-terraform-states"
     key                     = "aeternitystate.tfstate"
     region                  = "us-east-1"
-    shared_credentials_file = "/aws/credentials"
-    profile                 = "aeternity"
   }
 }
 
@@ -12,24 +10,18 @@ provider "aws" {
   version                 = "1.24"
   region                  = "ap-southeast-1"
   alias                   = "ap-southeast-1"
-  shared_credentials_file = "/aws/credentials"
-  profile                 = "aeternity"
 }
 
 provider "aws" {
   version                 = "1.24"
   region                  = "eu-central-1"
   alias                   = "eu-central-1"
-  shared_credentials_file = "/aws/credentials"
-  profile                 = "aeternity"
 }
 
 provider "aws" {
   version                 = "1.24"
   region                  = "us-west-2"
   alias                   = "us-west-2"
-  shared_credentials_file = "/aws/credentials"
-  profile                 = "aeternity"
 }
 
 module "aws_deploy-ap-southeast-1" {
