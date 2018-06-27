f7f4d474cc5043f51b05092f74d47b497ad12496
acid <acid@sennui.com>
2018-06-27 11:03:23 +0200

    fix syntax

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,27 @@
 terraform {
   backend "s3" {
-    bucket                  = "aeternity-terraform-states"
-    key                     = "aeternitystate.tfstate"
-    region                  = "us-east-1"
+    bucket = "aeternity-terraform-states"
+    key    = "aeternitystate.tfstate"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
-  version                 = "1.24"
-  region                  = "ap-southeast-1"
-  alias                   = "ap-southeast-1"
+  version = "1.24"
+  region  = "ap-southeast-1"
+  alias   = "ap-southeast-1"
 }
 
 provider "aws" {
-  version                 = "1.24"
-  region                  = "eu-central-1"
-  alias                   = "eu-central-1"
+  version = "1.24"
+  region  = "eu-central-1"
+  alias   = "eu-central-1"
 }
 
 provider "aws" {
-  version                 = "1.24"
-  region                  = "us-west-2"
-  alias                   = "us-west-2"
+  version = "1.24"
+  region  = "us-west-2"
+  alias   = "us-west-2"
 }
 
 module "aws_deploy-ap-southeast-1" {
