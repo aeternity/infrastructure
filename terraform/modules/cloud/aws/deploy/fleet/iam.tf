98523dd53ef79149feced70cd5b1c08b4f0e4053
Dincho Todorov <dincho@users.noreply.github.com>
2018-08-14 15:27:02 +0300

    Use dynamic cloud instance metadata instead of ansible vars

--- terraform/modules/cloud/aws/deploy/fleet/iam.tf
+++ terraform/modules/cloud/aws/deploy/fleet/iam.tf
@@ -36,6 +36,13 @@ resource "aws_iam_role_policy" "epoch_policy" {
       ],
       "Effect": "Allow",
       "Resource": "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:datadog_api_key*"
+    },
+    {
+      "Action": [
+        "ec2:DescribeTags"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
     }
   ]
 }
