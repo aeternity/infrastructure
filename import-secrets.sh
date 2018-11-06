cb128f6a3156bd5d3f0b3acddc3725cdf3329b48
Dincho Todorov <dincho@users.noreply.github.com>
2018-11-06 16:03:12 +0200

    Add secrets support for Google Cloud Platform

--- import-secrets.sh
+++ import-secrets.sh
@@ -2,6 +2,7 @@
 set -e
 
 AWS_CREDS_ROLE="${AWS_CREDS_ROLE:-epoch-fleet-manager}"
+CREDS_ROLE="${CREDS_ROLE:-$AWS_CREDS_ROLE}"
 
 if [ -n "$VAULT_GITHUB_TOKEN" ]; then
     export VAULT_TOKEN=$(vault login -field=token -method=github token=$VAULT_GITHUB_TOKEN)
@@ -18,13 +19,18 @@ if [ -n "$VAULT_AUTH_TOKEN" ]; then
 fi
 
 if [ -n "$VAULT_TOKEN" ]; then
-    AWS_CREDS=$(vault write -f aws/sts/${AWS_CREDS_ROLE})
+    AWS_CREDS=$(vault write -f aws/sts/${CREDS_ROLE})
     export AWS_ACCESS_KEY_ID=$(echo $AWS_CREDS | grep -o 'access_key [^ ]*' | awk '{print $2}')
     export AWS_SECRET_ACCESS_KEY=$(echo $AWS_CREDS | grep -o 'secret_key [^ ]*' | awk '{print $2}')
     export AWS_SESSION_TOKEN=$(echo $AWS_CREDS | grep -o 'security_token [^ ]*' | awk '{print $2}')
+
+    export GOOGLE_APPLICATION_CREDENTIALS=$HOME/.gcp.json
+    vault read -field json secret/google/${CREDS_ROLE} > $GOOGLE_APPLICATION_CREDENTIALS
+
     DOCKERHUB_CREDS=$(vault read secret/dockerhub/prod)
     export DOCKER_USER=$(echo $DOCKERHUB_CREDS | grep -o 'username [^ ]*' | awk '{print $2}')
     export DOCKER_PASS=$(echo $DOCKERHUB_CREDS | grep -o 'password [^ ]*' | awk '{print $2}')
+
     export DATADOG_API_KEY=$(vault read -field=api_key secret/datadog/deploy)
     export ROCKET_HOOK_URL=$(vault read -field=core-alerts secret/rocketchat/prod/hooks)
 fi
