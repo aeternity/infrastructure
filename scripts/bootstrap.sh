8f4d63493f5527ab520c9ccbf14f340521c3707e
acidsennui <36416344+acidsennui@users.noreply.github.com>
2018-11-23 08:09:12 +0100

    Bootstrap on reboot (#158)
    * move bootstrap to separated script it can be easy versioned

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# Do not run after first error
+set -e
+for i in "$@"
+do
+case $i in
+    --vault_addr=*)
+    vault_addr="${i#*=}"
+    shift # past argument=value
+    ;;
+    --vault_role=*)
+    vault_role="${i#*=}"
+    shift # past argument=value
+    ;;
+    --env=*)
+    env="${i#*=}"
+    shift # past argument=value
+    ;;
+    --epoch_package=*)
+    epoch_package="${i#*=}"
+    shift # past argument=value
+    ;;
+    --default)
+    DEFAULT=YES
+    shift # past argument with no value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+cd $(dirname $0)/../ansible/
+
+# Temporary fix/workaround for non-executable vault install
+chmod +x /usr/bin/vault
+
+# Authenticate the instance to CSM
+PKCS7=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/pkcs7 | tr -d '\n')
+
+export VAULT_ADDR=$vault_addr
+if [ -f "/root/.vault_nonce" ] ; then
+    export NONCE=$(cat /root/.vault_nonce)
+else
+    export NONCE=$(vault write auth/aws/login pkcs7=$PKCS7 role=$vault_role | grep token_meta_nonce | awk '{print $2}')
+    echo $NONCE > /root/.vault_nonce
+    chmod 0600 /root/.vault_nonce
+fi
+
+export VAULT_TOKEN=$(vault write -field=token auth/aws/login pkcs7=$PKCS7 role=$vault_role nonce=$NONCE)
+
+# Install ansible roles
+ansible-galaxy install -r requirements.yml
+
+# Create temporary inventory just because of the group_vars
+cat > /tmp/local_inventory << EOF
+[local]
+localhost ansible_connection=local
+
+[tag_role_epoch:children]
+local
+
+[tag_env_${env}:children]
+local
+
+EOF
+
+ansible-playbook \
+    -i /tmp/local_inventory \
+    -e env=${env} \
+    monitoring.yml
+
+ansible-playbook \
+    -i /tmp/local_inventory \
+    --become-user epoch -b \
+    -e package=${epoch_package} \
+    -e env=${env} \
+    -e db_version=0 \
+    deploy.yml
