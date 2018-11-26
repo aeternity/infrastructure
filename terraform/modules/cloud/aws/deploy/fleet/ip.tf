9454f73514aa11e49ec25c8fe623b2b77833e96b
acidsennui <acid@sennui.com>
2018-11-26 20:54:10 +0100

    add multi static

--- terraform/modules/cloud/aws/deploy/fleet/ip.tf
+++ terraform/modules/cloud/aws/deploy/fleet/ip.tf
@@ -1,9 +1,8 @@
 resource "aws_eip" "ip" {
   count = "${var.static_nodes}"
 }
-
 resource "aws_eip_association" "ip_associate" {
   count         = "${var.static_nodes}"
-  instance_id   = "${aws_instance.static_node.id}"
-  allocation_id = "${aws_eip.ip.id}"
+  instance_id   = "${aws_instance.static_node.*.id[count.index]}"
+  allocation_id = "${aws_eip.ip.*.id[count.index]}"
 }
