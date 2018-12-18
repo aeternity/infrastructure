6e06181adf10bffddd1af9d85312a9b036365af1
Dincho Todorov <dincho@users.noreply.github.com>
2018-12-18 11:21:13 +0200

    Fetch package from ec2 tags if not explicitly provided

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -77,6 +77,15 @@ local
 
 EOF
 
+# Fetch package from EC2 tags if not provided as parameter
+# Spot instances pass this parameter in user_data because they don't have tags assigned during boot yet
+# Static instances rely on tags
+if [ -z "$epoch_package" ]; then
+    INSTANCE_ID=$(ec2metadata --instance-id)
+    REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')
+    epoch_package=$(aws ec2 describe-tags --region=$REGION --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=key,Values=package" --query 'Tags[0].Value' --output=text)
+fi
+
 # While Ansible is run by Python 3 because of the virtual environment
 # the "remote" (which is in this case the same) host interpreter must also be set to python3
 # in this case it's the path in the virtual environment on the controller (same as the remote)
