546dc0d310b9a0b9206269b7fe2358214d46a1e1
Dincho Todorov <dincho@users.noreply.github.com>
2019-01-29 11:32:14 +0200

    Add next environments

--- terraform/main.tf
+++ terraform/main.tf
@@ -361,3 +361,47 @@ module "aws_deploy-integration-eu-west-2" {
     aws = "aws.eu-west-2"
   }
 }
+
+module "aws_deploy-next-eu-west-2" {
+  source            = "modules/cloud/aws/deploy"
+  env               = "next"
+  bootstrap_version = "master"
+  vault_role        = "ae-node"
+  vault_addr        = "${var.vault_addr}"
+
+  static_nodes  = 1
+  spot_nodes    = 2
+  spot_price    = "0.125"
+  instance_type = "m4.large"
+  ami_name      = "epoch-ubuntu-16.04-v1548669657"
+
+  aeternity = {
+    package = "https://s3.eu-central-1.amazonaws.com/aeternity-node-builds/aeternity-next-ubuntu-x86_64.tar.gz"
+  }
+
+  providers = {
+    aws = "aws.eu-west-2"
+  }
+}
+
+module "aws_deploy-unstable-eu-west-2" {
+  source            = "modules/cloud/aws/deploy"
+  env               = "unstable"
+  bootstrap_version = "master"
+  vault_role        = "ae-node"
+  vault_addr        = "${var.vault_addr}"
+
+  static_nodes  = 1
+  spot_nodes    = 2
+  spot_price    = "0.125"
+  instance_type = "m4.large"
+  ami_name      = "epoch-ubuntu-16.04-v1548669657"
+
+  aeternity = {
+    package = "https://s3.eu-central-1.amazonaws.com/aeternity-node-builds/aeternity-next-ubuntu-x86_64.tar.gz"
+  }
+
+  providers = {
+    aws = "aws.eu-west-2"
+  }
+}
