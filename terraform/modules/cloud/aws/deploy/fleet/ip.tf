519ddb768789474dad288aef0e864160d774bbcf
michal bagrowski <michal@bagrowski.com>
2019-02-14 13:31:26 +0100

    decrease donwtime with node rotation

--- terraform/modules/cloud/aws/deploy/fleet/ip.tf
+++ terraform/modules/cloud/aws/deploy/fleet/ip.tf
@@ -6,4 +6,8 @@ resource "aws_eip_association" "ip_associate" {
   count         = "${var.static_nodes}"
   instance_id   = "${aws_instance.static_node.*.id[count.index]}"
   allocation_id = "${aws_eip.ip.*.id[count.index]}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
