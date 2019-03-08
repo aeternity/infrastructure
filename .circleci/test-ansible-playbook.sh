8d5aa87eef39fad7684f4b4064b0e6ef2949a4d3
michal bagrowski <michal@bagrowski.com>
2019-03-08 14:35:21 +0100

    test

--- .circleci/test-ansible-playbook.sh
+++ .circleci/test-ansible-playbook.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 ansible-playbook -i localhost, \
-  -e ansible_python_interpreter=/usr/bin/python3 \
   -e ansible_user=root -e ansible_ssh_pass=root "$@"
 
 ansible-playbook -i localhost, \
-  -e ansible_python_interpreter=/usr/bin/python3 \
   -e ansible_user=root -e ansible_ssh_pass=root "$@" \
   | grep -q 'changed=0.*failed=0' \
   && (echo 'Idempotence test: pass' && exit 0) \
