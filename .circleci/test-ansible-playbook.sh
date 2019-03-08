457ababdd4e3fd2047fa832ea4acad3eafdb8c0b
michal bagrowski <michal@bagrowski.com>
2019-03-08 15:03:03 +0100

    setup secrets

--- .circleci/test-ansible-playbook.sh
+++ .circleci/test-ansible-playbook.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 ansible-playbook -i localhost, \
+  -e ansible_python_interpreter=/usr/bin/python3 \
   -e ansible_user=root -e ansible_ssh_pass=root "$@"
 
 ansible-playbook -i localhost, \
+  -e ansible_python_interpreter=/usr/bin/python3 \
   -e ansible_user=root -e ansible_ssh_pass=root "$@" \
   | grep -q 'changed=0.*failed=0' \
   && (echo 'Idempotence test: pass' && exit 0) \
