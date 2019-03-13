06c66df414f38774d0c89a59f3d591872bbb2ae5
Valeri Filipov <valeri.filpov@gmail.com>
2019-03-13 11:43:24 +0200

    fix temp filename

--- .circleci/test-ansible-playbook.sh
+++ .circleci/test-ansible-playbook.sh
@@ -4,7 +4,7 @@ ansible-playbook -i localhost, \
   -e ansible_python_interpreter=/usr/bin/python3 \
   -e ansible_user=root -e ansible_ssh_pass=root "$@"
 
-tempfile="/tmp/playbook-out-$(basename $@)-$(date +%s)"
+tempfile="/tmp/playbook-out-$(basename $1)-$(date +%s)"
 touch $tempfile
 
 ansible-playbook -i localhost, \
