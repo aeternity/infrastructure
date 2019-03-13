c6dbc314d1498648b6aba2d9dce1efe7f159c784
michal bagrowski <michal@bagrowski.com>
2019-03-13 10:25:39 +0100

    remove debug

--- .circleci/test-ansible-playbook.sh
+++ .circleci/test-ansible-playbook.sh
@@ -3,10 +3,6 @@
 ansible-playbook -i localhost, \
   -e ansible_python_interpreter=/usr/bin/python3 \
   -e ansible_user=root -e ansible_ssh_pass=root "$@"
-# just for testing to show ouput
-ansible-playbook -i localhost, \
-  -e ansible_python_interpreter=/usr/bin/python3 \
-  -e ansible_user=root -e ansible_ssh_pass=root "$@"
 
 ansible-playbook -i localhost, \
   -e ansible_python_interpreter=/usr/bin/python3 \
