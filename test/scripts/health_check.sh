8da4b99e935c4f862249d45cd6158e0752152bc9
Valeri Filipov <valeri.filpov@gmail.com>
2019-03-19 14:52:16 +0200

    add more checks combine fail output

--- test/scripts/health_check.sh
+++ test/scripts/health_check.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
-#example usage: test/scripts/health_check.sh --network=main --height=52000 --version=2.1.0 18.136.37.63
+#example usage: health_check.sh --network=main --min_height=52000 --version=2.1.0 18.136.37.63
 set -eo pipefail
 
-main_network_id=ae_mainnet
-uat_network_id=ae_uat
-main_genesis="kh_pbtwgLrNu23k9PA6XCZnUbtsvEFeQGgavY4FS2do3QP8kcp2z"
-uat_genesis="kh_wUCideEB8aDtUaiHCtKcfywU6oHZW6gnyci8Mw6S1RSTCnCRu"
-node_version=2.1.0
+declare -A network_genesis_hashmap
+network_genesis_hashmap['main']="kh_pbtwgLrNu23k9PA6XCZnUbtsvEFeQGgavY4FS2do3QP8kcp2z"
+network_genesis_hashmap['uat']="kh_wUCideEB8aDtUaiHCtKcfywU6oHZW6gnyci8Mw6S1RSTCnCRu"
+declare -A network_id_hashmap
+network_id_hashmap['main']=ae_mainnet
+network_id_hashmap['uat']=ae_uat
 
 for arg in "$@"; do
     case $arg in
         --network=*)
         network="${arg#*=}"
+        network_id=${network_id_hashmap[$network]}
+        genesis_hash=${network_genesis_hashmap[$network]}
         shift # past argument=value
         ;;
-        --height=*)
-        height="${arg#*=}"
-        shift # past argument=value
+        --network_id=*)
+        network_id="${arg#*=}"
+        shift
+        ;;
+        --min_height=*)
+        min_height="${arg#*=}"
+        shift
+        ;;
+        --genesis_hash=*)
+        genesis_hash="${arg#*=}"
+        shift
+        ;;
+        --min_sync_pct=*)
+        min_sync_pct="${arg#*=}"
+        shift
         ;;
         --version=*)
         version="${arg#*=}"
-        shift # past argument=value
+        shift
         ;;
         *)
               # unknown option
@@ -34,31 +49,48 @@ else
     HOST=${@:$#}
 fi
 
-network=${network:-main}
-height=${height:-0}
-version=${version:-$node_version}
-network_id=${network}_network_id
-network_genesis=${network}_genesis
+min_height=${min_height:-1}
 
 get_node_status() {
     curl -sS -m5 http://$HOST:3013/v2/status
 }
 check_genesis_hash() {
-    test $(echo $node_status| jq -r '.genesis_key_block_hash') == ${!network_genesis}
+    test $(echo $node_status| jq -r '.genesis_key_block_hash') == $genesis_hash
 }
 check_network_id() {
-    test $(echo $node_status| jq -r '.network_id') ==  ${!network_id}
+    test $(echo $node_status| jq -r '.network_id') == $network_id
 }
-check_top_height() {
-    test $(curl -sS -m5 http://$HOST:3013/v2/key-blocks/current | jq '.height') -gt $height
+check_sync_progress() {
+    echo "$(echo $node_status| jq -r '.sync_progress')>=$min_sync_pct"|bc
+}
+check_top_min_height() {
+    test $(curl -sS -m5 http://$HOST:3013/v2/key-blocks/current | jq '.height') -ge $min_height
 }
 check_version() {
     test $(echo $node_status| jq -r '.node_version') == $version
 }
 
 node_status=$(get_node_status)
+check_top_min_height || failed+=" min_height"
 
-check_genesis_hash || (echo "genesis check failed"; exit 1)
-check_top_height || (echo "height check failed"; exit 1)
-check_network_id || (echo "network check failed"; exit 1)
-check_version || (echo "version check failed"; exit 1)
+if [ -n "$genesis_hash" ]; then
+    check_genesis_hash || failed+=" genesis"
+fi
+if [ -n "$network_id" ]; then
+    check_network_id || failed+=" network_id"
+fi
+if [ -n "$version" ]; then
+    check_version || failed+=" version"
+fi
+if [ -n "$min_sync_pct" ]; then
+    if [ "$(check_sync_progress)" -ne 1 ]; then
+       failed+=" sync_progress"
+    fi
+fi
+
+if [ "$failed" != "" ]; then
+    for fail in $failed; do
+        echo "$fail" check failed
+    done
+    exit 1
+fi
