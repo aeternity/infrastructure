8b4ff8904d3ff4d161a613ce569487b07db17dfe
michal bagrowski <michal@bagrowski.com>
2019-03-20 14:39:50 +0100

    add tag kind->api

--- terraform/modules/cloud/aws/deploy/fleet/main.tf
+++ terraform/modules/cloud/aws/deploy/fleet/main.tf
@@ -105,6 +105,11 @@ resource "aws_autoscaling_group" "spot_fleet" {
 
   tags = [
     {
+      key                 = "kind"
+      value               = "api"
+      propagate_at_launch = true
+    },
+    {
       key                 = "Name"
       value               = "ae-${var.env}-nodes"
       propagate_at_launch = true
