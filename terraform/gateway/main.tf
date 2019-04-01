3377732725e738d6e69df17be2530a39c9a4b2df
michal bagrowski <michal@bagrowski.com>
2019-04-01 12:38:18 +0200

    cleanup

--- terraform/gateway/main.tf
+++ terraform/gateway/main.tf
@@ -3,7 +3,7 @@ variable "vault_addr" {
 }
 
 variable "bootstrap_version" {
-  default = "close_3013_for_public_for_api_gateway_164955096"
+  default = "stable"
 }
 
 variable "main_gateway_dns" {
@@ -19,8 +19,8 @@ module "aws_deploy-main-us-west-2" {
 
   static_nodes      = 0
   spot_nodes        = 0
-  gateway_nodes_min = 1
-  gateway_nodes_max = 1
+  gateway_nodes_min = 2
+  gateway_nodes_max = 30
   dns_zone          = "${var.dns_zone}"
   gateway_dns       = "${var.main_gateway_dns}"
   spot_price        = "0.15"
@@ -46,8 +46,8 @@ module "aws_deploy-main-eu-north-1" {
 
   static_nodes      = 0
   spot_nodes        = 0
-  gateway_nodes_min = 1
-  gateway_nodes_max = 1
+  gateway_nodes_min = 2
+  gateway_nodes_max = 30
   dns_zone          = "${var.dns_zone}"
   gateway_dns       = "${var.main_gateway_dns}"
 
