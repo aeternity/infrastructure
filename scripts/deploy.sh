020838628d4a236c04d64efb1171dc049cce77bd
Dincho Todorov <dincho@users.noreply.github.com>
2020-01-27 15:23:41 +0200

    Add API gateways to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -25,6 +25,7 @@ if [[ $mainchoice == "y" ]]; then
     DEPLOY_ENV=main DEPLOY_DOWNTIME=600 ROLLING_UPDATE=30% make deploy
 fi
 
+# Monitoring nodes
 read -n 1 -p "Deploy UAT monitoring nodes? (y/N):" uatmonchoice
 if [[ $uatmonchoice == "y" ]]; then
     DEPLOY_ENV=uat_mon DEPLOY_REGION=ap-southeast-1 CONFIG_ENV=uat_mon@ap-southeast-1 make deploy
@@ -41,6 +42,7 @@ if [[ $mainmonchoice == "y" ]]; then
     DEPLOY_ENV=main_mon DEPLOY_REGION=us-east-2 CONFIG_ENV=main_mon@us-east-2 make deploy
 fi
 
+# Backup nodes
 read -n 1 -p "Deploy UAT backup nodes? (y/N):" backupuatchoice
 if [[ $backupuatchoice == "y" ]]; then
     DEPLOY_ENV=uat DEPLOY_KIND=backup make deploy
@@ -49,6 +51,33 @@ fi
 read -n 1 -p "Deploy MAIN backup nodes? (y/N):" backupmainchoice
 if [[ $backupmainchoice == "y" ]]; then
     DEPLOY_ENV=main DEPLOY_KIND=backup make deploy
+fi
+
+# Testnet gateway nodes
+read -n 1 -p "Deploy testnet API gateway - Stockholm? (y/N):" testnetgate1
+if [[ $testnetgate1 == "y" ]]; then
+    DEPLOY_ENV=api_uat DEPLOY_REGION=eu_north_1 make deploy
+fi
+
+read -n 1 -p "Deploy testnet API gateway - Singapore? (y/N):" testnetgate2
+if [[ $testnetgate2 == "y" ]]; then
+    DEPLOY_ENV=api_uat DEPLOY_REGION=ap_southeast_1 make deploy
+fi
+
+# Mainnet gateway nodes
+read -n 1 -p "Deploy mainnet API gateway - Stockholm? (y/N):" mainnetgate1
+if [[ $mainnetgate1 == "y" ]]; then
+    DEPLOY_ENV=api_main DEPLOY_REGION=eu_north_1 make deploy
+fi
+
+read -n 1 -p "Deploy mainnet API gateway - Singapore? (y/N):" mainnetgate2
+if [[ $mainnetgate2 == "y" ]]; then
+    DEPLOY_ENV=api_main DEPLOY_REGION=ap_southeast_1 make deploy
+fi
+
+read -n 1 -p "Deploy mainnet API gateway - Oregon? (y/N):" mainnetgate3
+if [[ $mainnetgate3 == "y" ]]; then
+    DEPLOY_ENV=api_main DEPLOY_REGION=us-west-2 make deploy
 fi
 
 # restore the working dir
