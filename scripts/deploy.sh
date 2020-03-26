6bf957afe8f6e5a1469930d084226a99a505c61a
Ivaylo Papazov <papazov@vmandco.com>
2020-03-26 12:53:09 +0200

    Add default deploy version
    Remove limit of characters length

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,25 +8,25 @@ cd $ROOT_DIR
 pwd
 
 export DEPLOY_DB_VERSION=1
-export PACKAGE=https://releases.aeternity.io/aeternity-${DEPLOY_VERSION:?}-ubuntu-x86_64.tar.gz
+export PACKAGE=https://releases.aeternity.io/aeternity-${DEPLOY_VERSION=latest}-ubuntu-x86_64.tar.gz
 
-read -n 1 -p "Deploy blue UAT nodes? (y/N):" blueuatchoice
+read -p "Deploy blue UAT nodes? (y/N):" blueuatchoice
 if [[ $blueuatchoice == "y" ]]; then
     DEPLOY_ENV=uat DEPLOY_COLOR=blue DEPLOY_DOWNTIME=600 make deploy
 fi
 
-read -n 1 -p "Deploy green UAT nodes? (y/N):" greenuatchoice
+read -p "Deploy green UAT nodes? (y/N):" greenuatchoice
 if [[ $greenuatchoice == "y" ]]; then
     DEPLOY_ENV=uat DEPLOY_COLOR=green DEPLOY_DOWNTIME=600 make deploy
 fi
 
-read -n 1 -p "Deploy MAIN nodes? (y/N):" mainchoice
+read -p "Deploy MAIN nodes? (y/N):" mainchoice
 if [[ $mainchoice == "y" ]]; then
     DEPLOY_ENV=main DEPLOY_DOWNTIME=600 ROLLING_UPDATE=30% make deploy
 fi
 
 # Monitoring nodes
-read -n 1 -p "Deploy UAT monitoring nodes? (y/N):" uatmonchoice
+read -p "Deploy UAT monitoring nodes? (y/N):" uatmonchoice
 if [[ $uatmonchoice == "y" ]]; then
     DEPLOY_ENV=uat_mon DEPLOY_REGION=ap-southeast-1 CONFIG_ENV=uat_mon@ap-southeast-1 make deploy
     DEPLOY_ENV=uat_mon DEPLOY_REGION=eu-central-1 CONFIG_ENV=uat_mon@eu-central-1 make deploy
@@ -34,7 +34,7 @@ if [[ $uatmonchoice == "y" ]]; then
     DEPLOY_ENV=uat_mon DEPLOY_REGION=us-west-2 CONFIG_ENV=uat_mon@us-west-2 make deploy
 fi
 
-read -n 1 -p "Deploy MAIN monitoring nodes? (y/N):" mainmonchoice
+read -p "Deploy MAIN monitoring nodes? (y/N):" mainmonchoice
 if [[ $mainmonchoice == "y" ]]; then
     DEPLOY_ENV=main_mon DEPLOY_REGION=ap-southeast-1 CONFIG_ENV=main_mon@ap-southeast-1 make deploy
     DEPLOY_ENV=main_mon DEPLOY_REGION=eu-north-1 CONFIG_ENV=main_mon@eu-north-1 make deploy
@@ -43,39 +43,39 @@ if [[ $mainmonchoice == "y" ]]; then
 fi
 
 # Backup nodes
-read -n 1 -p "Deploy UAT backup nodes? (y/N):" backupuatchoice
+read -p "Deploy UAT backup nodes? (y/N):" backupuatchoice
 if [[ $backupuatchoice == "y" ]]; then
     DEPLOY_ENV=uat DEPLOY_KIND=backup make deploy
 fi
 
-read -n 1 -p "Deploy MAIN backup nodes? (y/N):" backupmainchoice
+read -p "Deploy MAIN backup nodes? (y/N):" backupmainchoice
 if [[ $backupmainchoice == "y" ]]; then
     DEPLOY_ENV=main DEPLOY_KIND=backup make deploy
 fi
 
 # Testnet gateway nodes
-read -n 1 -p "Deploy testnet API gateway - Stockholm? (y/N):" testnetgate1
+read -p "Deploy testnet API gateway - Stockholm? (y/N):" testnetgate1
 if [[ $testnetgate1 == "y" ]]; then
     DEPLOY_ENV=api_uat DEPLOY_REGION=eu_north_1 make deploy
 fi
 
-read -n 1 -p "Deploy testnet API gateway - Singapore? (y/N):" testnetgate2
+read -p "Deploy testnet API gateway - Singapore? (y/N):" testnetgate2
 if [[ $testnetgate2 == "y" ]]; then
     DEPLOY_ENV=api_uat DEPLOY_REGION=ap_southeast_1 make deploy
 fi
 
 # Mainnet gateway nodes
-read -n 1 -p "Deploy mainnet API gateway - Stockholm? (y/N):" mainnetgate1
+read -p "Deploy mainnet API gateway - Stockholm? (y/N):" mainnetgate1
 if [[ $mainnetgate1 == "y" ]]; then
     DEPLOY_ENV=api_main DEPLOY_REGION=eu_north_1 make deploy
 fi
 
-read -n 1 -p "Deploy mainnet API gateway - Singapore? (y/N):" mainnetgate2
+read -p "Deploy mainnet API gateway - Singapore? (y/N):" mainnetgate2
 if [[ $mainnetgate2 == "y" ]]; then
     DEPLOY_ENV=api_main DEPLOY_REGION=ap_southeast_1 make deploy
 fi
 
-read -n 1 -p "Deploy mainnet API gateway - Oregon? (y/N):" mainnetgate3
+read -p "Deploy mainnet API gateway - Oregon? (y/N):" mainnetgate3
 if [[ $mainnetgate3 == "y" ]]; then
     DEPLOY_ENV=api_main DEPLOY_REGION=us-west-2 make deploy
 fi
