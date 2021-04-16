2c7bb5bfd2745065de59b5b5497687f05bc2487b
Dincho Todorov <dincho@users.noreply.github.com>
2021-04-16 15:01:18 +0300

    Add apt-ci role (#530)

---
+++ scripts/secrets/policies/apt-ci.sh
@@ -0,0 +1,12 @@
+VAULT_SECRETS_ROLE=${VAULT_SECRETS_ROLE:-apt-ci}
+
+AWS_CREDS=$(vault write -f aws/sts/${VAULT_SECRETS_ROLE} ttl=${VAULT_TOKENS_TTL})
+AWS_ACCESS_KEY_ID=$(echo $AWS_CREDS | grep -o 'access_key [^ ]*' | awk '{print $2}')
+AWS_SECRET_ACCESS_KEY=$(echo $AWS_CREDS | grep -o 'secret_key [^ ]*' | awk '{print $2}')
+AWS_SESSION_TOKEN=$(echo $AWS_CREDS | grep -o 'security_token [^ ]*' | awk '{print $2}')
+
+dump_var "VAULT_ADDR"
+dump_var "VAULT_TOKEN"
+dump_var "AWS_ACCESS_KEY_ID"
+dump_var "AWS_SECRET_ACCESS_KEY"
+dump_var "AWS_SESSION_TOKEN"
