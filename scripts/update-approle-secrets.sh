7c89ddd88fbc48011c268eef5adb22be339fa299
Dincho Todorov <dincho@users.noreply.github.com>
2021-10-14 16:05:35 +0300

    Add apt-ci role to secrets uprading script (#536)

--- scripts/update-approle-secrets.sh
+++ scripts/update-approle-secrets.sh
@@ -28,6 +28,7 @@ export CIRCLECI_TOKEN=$(vault read -field=token secret/circleci)
 update_circleci node-ci 81741cd0-d6ba-4380-8c8a-49c625fc7ca2
 update_circleci infra-ci 167aeaef-d241-4b47-8bb5-86f4fa785101
 update_circleci node-images-ci a3ab7a2d-75ff-44f9-8c9f-016e66850ed8
+update_circleci apt-ci 96ec8d55-4a50-4f0a-8337-b0a7e0443a62
 
 # Update SSM
 SECRET_ID=$(vault write -field=secret_id -f auth/approle/role/batch-jobs/secret-id)
