cec583938ebd3eabf30382cbaa471b7623ec0972
Michal `acid` Bagrowski <michal@bagrowski.com>
2022-02-07 22:07:54 +0100

    add test in correct way

--- test/terraform/test.tf
+++ test/terraform/test.tf
@@ -45,7 +45,7 @@ module "aws_deploy-test-ubuntu-bionic" {
 
   spot_price    = "0.04"
   instance_type = "t3.large"
-  ami_name      = "aeternity-ubuntu-${var.ubuntu-version}"
+  ami_name      = "aeternity-ubuntu-18.04"
 
   additional_storage      = true
   additional_storage_size = 5
@@ -56,6 +56,29 @@ module "aws_deploy-test-ubuntu-bionic" {
 }
 
 
-variable "ubuntu-version" {
-  default = "18.04"
+module "aws_deploy-test-ubuntu-focal" {
+  source              = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
+  env                 = var.env_name
+  envid               = var.envid
+  bootstrap_version   = var.bootstrap_version
+  vault_role          = "ae-node"
+  vault_addr          = var.vault_addr
+  user_data_file      = "user_data.bash"
+  node_config         = "secret/aenode/config/test"
+
+  static_nodes = 1
+  spot_nodes   = 1
+
+  aeternity =  var.aeternity
+
+  spot_price    = "0.04"
+  instance_type = "t3.large"
+  ami_name      = "aeternity-ubuntu-20.04"
+
+  additional_storage      = true
+  additional_storage_size = 5
+
+  providers = {
+    aws = "aws.ap-southeast-2"
+  }
 }
