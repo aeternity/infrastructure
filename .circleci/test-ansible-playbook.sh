b41a5bf58f3566fa65e6b40db1bff4276335110b
Dincho Todorov <dincho@users.noreply.github.com>
2023-12-05 15:02:16 +0200

    Use a named test container: aenode

--- .circleci/test-ansible-playbook.sh
+++ .circleci/test-ansible-playbook.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-ansible-playbook -i localhost, \
+ansible-playbook -i aenode, \
   -e ansible_python_interpreter=/usr/bin/python3 \
   -e ansible_user=root -e ansible_ssh_pass=root "$@"
 
 tempfile="/tmp/playbook-out-$(basename $1)-$(date +%s)"
 touch $tempfile
 
-ansible-playbook -i localhost, \
+ansible-playbook -i aenode, \
   -e ansible_python_interpreter=/usr/bin/python3 \
   -e ansible_user=root -e ansible_ssh_pass=root "$@" \
   | tee $tempfile \
