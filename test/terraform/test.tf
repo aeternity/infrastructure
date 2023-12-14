6083505771a6ba2a67b673c0a0cd2cfa3b63f226
Dincho Todorov <dincho@users.noreply.github.com>
2023-12-14 10:24:52 +0200

    Remote 2204 not yet ready integration tests

--- test/terraform/test.tf
+++ test/terraform/test.tf
@@ -52,39 +52,6 @@ module "test-aenode-1804" {
   }
 }
 
-module "test-aenode-2204" {
-  source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v3.1.0"
-  env    = var.env_name
-
-  static_nodes = 1
-  spot_nodes   = 1
-
-  instance_type  = "t3.large"
-  instance_types = ["t3.large"]
-  ami_name       = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"
-
-  additional_storage      = true
-  additional_storage_size = 5
-
-  tags = {
-    env   = var.env_name
-    envid = var.envid
-    role  = "aenode"
-    kind  = "2204"
-  }
-
-  config_tags = {
-    vault_addr        = var.vault_addr
-    vault_role        = "ae-node"
-    bootstrap_version = var.bootstrap_version
-    bootstrap_config  = "secret/aenode/config/${var.env_name}"
-  }
-
-  providers = {
-    aws = "aws.ap-southeast-2"
-  }
-}
-
 module "test-aemdw-1804" {
   source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v3.1.0"
   env    = var.env_name
@@ -106,41 +73,6 @@ module "test-aemdw-1804" {
     envid = var.envid
     role  = "aemdw"
     kind  = "1804"
-  }
-
-  config_tags = {
-    vault_addr        = var.vault_addr
-    vault_role        = "ae-node"
-    bootstrap_version = var.bootstrap_version
-    bootstrap_config  = "secret/aenode/config/${var.env_name}"
-  }
-
-  providers = {
-    aws = "aws.ap-southeast-2"
-  }
-}
-
-module "test-aemdw-2204" {
-  source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v3.1.0"
-  env    = var.env_name
-
-  static_nodes = 1
-  spot_nodes   = 0
-
-  instance_type  = "t3.large"
-  instance_types = ["t3.large"]
-  ami_name       = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"
-
-  additional_storage      = true
-  additional_storage_size = 5
-
-  enable_mdw = true
-
-  tags = {
-    env   = var.env_name
-    envid = var.envid
-    role  = "aemdw"
-    kind  = "2204"
   }
 
   config_tags = {
