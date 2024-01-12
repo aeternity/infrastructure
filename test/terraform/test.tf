b507020600a77157411dc0453a19d80faaa57a67
Dincho Todorov <dincho@users.noreply.github.com>
2024-01-12 09:24:13 +0200

    Add Ubuntu 22.04 integration tests

--- test/terraform/test.tf
+++ test/terraform/test.tf
@@ -52,6 +52,39 @@ module "test-aenode-1804" {
   }
 }
 
+module "test-aenode-2204" {
+  source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v3.1.0"
+  env    = var.env_name
+
+  static_nodes = 1
+  spot_nodes   = 1
+
+  instance_type  = "t3.large"
+  instance_types = ["t3.large"]
+  ami_name       = "aeternity-ubuntu-22.04-*"
+
+  additional_storage      = true
+  additional_storage_size = 5
+
+  tags = {
+    env   = var.env_name
+    envid = var.envid
+    role  = "aenode"
+    kind  = "2204"
+  }
+
+  config_tags = {
+    vault_addr        = var.vault_addr
+    vault_role        = "ae-node"
+    bootstrap_version = var.bootstrap_version
+    bootstrap_config  = "secret/aenode/config/${var.env_name}"
+  }
+
+  providers = {
+    aws = "aws.ap-southeast-2"
+  }
+}
+
 module "test-aemdw-1804" {
   source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v3.1.0"
   env    = var.env_name
